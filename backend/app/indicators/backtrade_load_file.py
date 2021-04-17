95f939ce8e9a097e1484d5905054f89c0061735b
himanshueip <naithani.himanshu53@gmail.com>
2021-04-17 14:24:30 +0530

    added dynamic array for symbols list and startegy list

--- backend/app/indicators/backtrade_load_file.py
+++ backend/app/indicators/backtrade_load_file.py
@@ -1,32 +1,61 @@
 from datetime import datetime
 import backtrader as bt
-from dynamic_params as *
+import importlib
+from dynamic import DynamicArray
+#moduleName = input('dynamic')
+importlib.import_module('dynamic')
 
-//memory allocation
+#memory allocation
 STRATEGY = DynamicArray()
 # Append new element
-STRATEGY.append(SmaCross)
-len(STRATEGY)
+STRATEGY.append("SmaCross")
+stra_id=len(STRATEGY)
 
 STOCK_ID = DynamicArray()
 # Append new element
-STOCK_ID.append("resources\MAHABANK.NS.csv")
-len(STOCK_ID)
+STOCK_ID.append("..\\resources\\MAHABANK.NS.csv")
+stock_number=len(STOCK_ID)
 
 
+class SmaCross(bt.Strategy):
+    # list of parameters which are configurable for the strategy
+    params = dict(
+        pfast=10,  # period for the fast moving average
+        pslow=30   # period for the slow moving average
+    )
+
+    def __init__(self):
+        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
+        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
+        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+
+    def next(self):
+        if not self.position:  # not in the market
+            if self.crossover > 0:  # if fast crosses slow to the upside
+                self.buy()  # enter long
+
+        elif self.crossover < 0:  # in the market & cross to the downside
+            self.close()  # close long position
 
 
-
-
-
-data = bt.feeds.YahooFinanceData(dataname=STOCK_ID,
-                                 fromdate=datetime(2020, 5, 6),
-                                 todate=datetime(2021, 3, 23))
-
+print(STOCK_ID[0])
+data = bt.feeds.YahooFinanceData(dataname=STOCK_ID[0],
+                                 fromdate=datetime(2020, 1, 1),
+                                 todate=datetime(2021, 3, 21))
 
 cerebro = bt.Cerebro() 
+cerebro.adddata(data)  # Add the data feed
+
+'''
 cerebro.addanalyzer(bt.analyzers.Returns)
 cerebro.optstrategy(VolumeWeightedAveragePrice, idx=[0, 1])
 results = cerebro.run(maxcpus=args.maxcpus, optreturn=args.optreturn)
 cerebro.adddata(data)  # Add the data feed
 cerebro.addstrategy(STRATEGY,pfast=10,pslow=40)  # Add the trading strategy
+'''
+
+cerebro.addstrategy(SmaCross)  # Add the trading strategy
+cerebro.run()  # run it all
+cerebro.plot()  # and plot it with a single command
+
+
