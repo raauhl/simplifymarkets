bd0abe42641b1ee1bdb875fabdc8014667452a45
himanshueip <naithani.himanshu53@gmail.com>
2021-04-18 18:42:30 +0530

    cleanup code

--- backend/app/indicators/backtrade_load_file.py
+++ backend/app/indicators/backtrade_load_file.py
@@ -16,31 +16,40 @@ STOCK_ID.append("..\\resources\\MAHABANK.NS.csv")
 
 
 
+if __name__ == '__main__':
 
+    cerebro = bt.Cerebro(optreturn=False)
+
+    cerebro.addstrategy(SmaCross,pslow=10,pfast=30)
 #Set data parameters and add to Cerebro
 data = bt.feeds.YahooFinanceCSVData(
     dataname=STOCK_ID[0],
-    fromdate=datetime.datetime(2020, 5, 5),
-    timeframe=bt.TimeFrame.Days,
-    todate=datetime.datetime(2020, 6, 12))
+    fromdate=datetime.datetime(2020, 4, 24),
+    #timeframe=bt.TimeFrame.Days,
+    todate=datetime.datetime(2021, 4, 12))
     #settings for out-of-sample data
     #fromdate=datetime.datetime(2018, 1, 1),
     #todate=datetime.datetime(2019, 12, 25))
 
+#added resample data for converting to weeks
+#cerebro.resampledata(data, timeframe=bt.TimeFrame.Weeks, name='weeks')
 
-#starting cerebro engine
-cerebro = bt.Cerebro(optreturn=False)
+# added logger
 cerebro.addwriter(bt.WriterFile, csv=True, out="trade_history.csv")
+
+#adding data
 cerebro.adddata(data)
-cerebro.addstrategy(btstrats.SMA_CrossOver)
+
+
+#cerebro.optstrategy(SMA_CrossOver, period=xrange(10, 30), factor=3.5)
 
 # Analyzer
-cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='mysharpe')
+#cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='mysharpe')
 
-thestrats = cerebro.run()
-thestrat = thestrats[0]
+cerebro.run()
+#thestrat = thestrats[0]
 
 print('Sharpe Ratio:', thestrat.analyzers.mysharpe.get_analysis())
 #cerebro.addstrategy(STRATEGY[0],pfast=10,pslow=30)
 #cerebro.run()
-#cerebro.plot()
+#cerebro.plot() 
